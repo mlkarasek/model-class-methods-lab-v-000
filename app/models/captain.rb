685c0ce7bdf320bf3d696e4acfe06fb733681529
Monica  Karasek <mlkarasek@gmail.com>
2018-08-28 00:42:15 +0000

    automatically backed up by learn

--- app/models/captain.rb
+++ app/models/captain.rb
@@ -2,15 +2,15 @@ class Captain < ActiveRecord::Base
   has_many :boats
 
   def self.catamaran_operators
-    includes(boats: :classifications).where("classifications.name = ?", "Catamaran")
-  end
+     includes(boats: :classifications).where(classifications: {name: "Catamaran"})
+   end 
 
   def self.sailors
-    includes(boats: :classifications).where("classifications.name = ?", "Sailboat")
+    includes(boats: :classifications).where(classifications: {name: "Sailboat")
   end
 
   def self.motorboats
-    includes(boats: :classifications).where("classifications.name = ?", "Motorboat")
+    includes(boats: :classifications).where(classifications: {name: "Motorboat")
 
   def self.talented_seafarers
     where("id IN (?)", self.sailors.pluck(:id) & self.motorboats.pluck(:id))
